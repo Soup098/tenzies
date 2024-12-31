7977634024d0fb07d7e635433aa80338582dd9d4
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-12-30 21:51:59 -0700

    Update

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,36 @@
+import Die from "../components/Die.jsx"
+import { useState } from "react"
+
+
 
 export default function App(){
+
+    
+
+    const [dieValue, setDieValue] = useState(generateAllNewDice)
+
+    const dice = dieValue.map((value) => <Die value={value}/>)
+
+    function generateAllNewDice(){
+        let diceArray = []
+        while(diceArray.length < 10){
+            let newDie = Math.floor(Math.random()*6)+1
+            diceArray.push(newDie)
+        }
+        return(diceArray)
+    }
+
+    function handleClick(){
+        setDieValue(generateAllNewDice)
+    }
+
     return(
-        <main></main>
+        <main>
+            <div className="dice-container">
+                {dice}
+            </div>
+            <button className="roll-btn" onClick={handleClick}>Roll</button>
+        </main>
     )
 }
     
