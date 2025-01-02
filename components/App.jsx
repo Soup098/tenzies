d02f8679d79d5d92c3fdf54eb714895da45ac559
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-01-01 23:04:12 -0700

    update

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,13 @@
 import Die from "../components/Die.jsx"
 import { useState } from "react"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
+//main app component
 export default function App(){
+    //initiate the app state with the array of dice objects
     const [dice, setDice] = useState(generateAllNewDice)
-
+    //create variable called diceElements that is an array of <Die /> components using the values of the dice array for props. rendered to the page in the App() return
     const diceElements = dice.map((dieObject) => (
             <Die 
                 key={dieObject.id} 
@@ -14,7 +17,10 @@ export default function App(){
                 id={dieObject.id}
             />
     ))
-
+    //logic to switch the gameWon boolean to true if all dice are held and have the same value
+    const gameWon = dice.every(die => die.isHeld) &&
+                dice.every(die => die.value === dice[0].value)
+    //function to generate an array of 10 dice objects with random number values, and initiate an isHeld value of false. called by the useState() on page render
     function generateAllNewDice(){
         let diceArray = []
         while(diceArray.length < 10){
@@ -27,15 +33,20 @@ export default function App(){
         }
         return(diceArray)
     }
-    function handleClick(){
+    //function for the roll button. if the die isHeld boolean value is true, the die does not change face value, otherwise, it does.
+    function rollDice(){
+        gameWon
+            ? setDice(generateAllNewDice)
+            :
         setDice(prevDice =>
             prevDice.map(die =>
                 die.isHeld
-                    ? {...die, value: die.value}
+                    ? die
                     : {...die, value: Math.floor(Math.random()*6)+1}
             )
         )
     }
+    //function to toggle the isHeld value of the <Die/> component. sent to component via props.
     function holdDie(id){
         setDice(prevDice => 
             prevDice.map(die =>
@@ -47,10 +58,18 @@ export default function App(){
 
     return(
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll the dice until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-btn" onClick={handleClick}>Roll</button>
+            <button className="roll-btn" onClick={rollDice}>
+                {gameWon ? "New Game" : "Roll"}
+            </button>
+            {gameWon 
+                ?   <Confetti height={"1080px"}/>
+                :null
+                }
         </main>
     )
 }
--- components/Die.jsx
+++ components/Die.jsx
@@ -1,8 +1,9 @@
 export default function Die(props){
+    //change the style of the die based on the isHeld value
     const styles ={
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
-
+    // function to call function from props. to be referenced in the return. 
     function holdDie(){
         return props.hold(props.id)
     }
@@ -11,7 +12,7 @@ export default function Die(props){
          <button 
             className="die" 
             style={styles} 
-            onClick={holdDie}
+            onClick={holdDie}// reference to the holdDie function.
             >
             {props.value}
         </button>
